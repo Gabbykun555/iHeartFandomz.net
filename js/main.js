d33911d7266c36d08812615bd2ed363d655d4e4f
Gabby Rutherford <gabbyrutherford@gmail.com>
2017-12-05 01:40:42 -0600

    Fanart page added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    $('.thumbnails').mouseenter(function() {
+   $('.thumbnails').mouseenter(function() {
        $(this).addClass('thumbnail-hover')
 });   
     $('.thumbnails').mouseleave(function() {
        $(this).removeClass('thumbnail-hover');
-    });
+    }); 
     
     $('ul li').click(function() {
      $(this).addClass('active').siblings('li').removeClass('active');
